664a2b43cb62a121d518c7da5cf7171ab7273ffe
Quang Nguyen <quangn2595@gmail.com>
2019-10-13 10:28:31 -0700

    initialize the project

---
+++ src/main/java/com/focalpoint/Focalist/FocalistApplication.java
@@ -0,0 +1,13 @@
+package com.focalpoint.Focalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocalistApplication.class, args);
+	}
+
+}
