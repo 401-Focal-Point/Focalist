3495c354a796dbbc6e52c142821ab7e70c236ebc
ahren <ahrenswett@gmail.com>
2019-10-15 15:30:29 -0700

    update remove else statement and order by query

--- src/main/java/com/focalpoint/Focalist/models/TaskRepository.java
+++ src/main/java/com/focalpoint/Focalist/models/TaskRepository.java
@@ -1,9 +1,11 @@
 package com.focalpoint.Focalist.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-   List<Task> OrderByUtcTime();
+   @Query(value = "SELECT * FROM task u ORDER BY u.utc_time ASC", nativeQuery = true)
+   List<Task> findAllOrderByUtcTime();
 }
