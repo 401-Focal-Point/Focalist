d506680f840c6ca2824fb3c1c6724170c1bf61e8
Quang Nguyen <quangn2595@gmail.com>
2019-10-14 17:36:23 -0700

    got time to save in database as utc

--- src/main/java/com/focalpoint/Focalist/controllers/TaskController.java
+++ src/main/java/com/focalpoint/Focalist/controllers/TaskController.java
@@ -4,6 +4,7 @@ import com.focalpoint.Focalist.models.ApplicationUser;
 import com.focalpoint.Focalist.models.ApplicationUserRepository;
 import com.focalpoint.Focalist.models.Task;
 import com.focalpoint.Focalist.models.TaskRepository;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,13 +28,19 @@ public class TaskController {
     @PostMapping("/api/task")
     public RedirectView addTask(String title, String note, String time, String offset, Principal p) {
         ApplicationUser currentUser = applicationUserRepository.findByUsername(p.getName());
-        time = time + "+0" + offset + ":00";
+//        time = time + "+0" + offset + ":00";
         System.out.println(time);
-        OffsetDateTime taskTime = OffsetDateTime.parse(time);
+        DateTime taskTime = DateTime.parse(time);
         System.out.println(taskTime);
-        Date UtcTime = Date.from(taskTime.toInstant());
-        System.out.println(UtcTime);
-        Task newTask = new Task(title, note, UtcTime, currentUser);
+        int offsetHours = Integer.parseInt(offset);
+        System.out.println(offsetHours);
+        taskTime = taskTime.plusHours(offsetHours);
+        System.out.println(taskTime);
+//        DateTime UtcTime = DateTime.(taskTime.toInstant());
+//        System.out.println(UtcTime);
+        Date taskUtcTime = taskTime.toDate();
+        System.out.println(taskUtcTime);
+        Task newTask = new Task(title, note, taskUtcTime, currentUser);
         taskRepository.save(newTask);
         return new RedirectView("/userAccount");
     }
