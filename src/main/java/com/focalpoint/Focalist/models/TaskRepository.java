40954bcebb97a44074a565aa15d4250c8be57963
ahren <ahrenswett@gmail.com>
2019-10-15 12:54:15 -0700

    logic change in message sender route

--- src/main/java/com/focalpoint/Focalist/models/TaskRepository.java
+++ src/main/java/com/focalpoint/Focalist/models/TaskRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-   List<Task> findByApplicationUserIdOrderByUtcTime(long id);
+   List<Task> OrderByUtcTime();
 }
