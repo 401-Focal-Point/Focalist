185df955b7e91f4c4ac7737000db65849a439876
Quang Nguyen <quangn2595@gmail.com>
2019-10-19 10:32:21 -0700

    fixed issue with app.js file not being read

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,20 +3,19 @@
 // pass the offset to a hidden input that will get passed along with the form for adding tasks.
 // Michelle dont hack the hidden input for offset. Just pretend it is actually hidden.
 (function() {
-    var offset = new Date().getTimezoneOffset()/60;
-    $('#offset').val(offset);
+	var offset = new Date().getTimezoneOffset() / 60;
+	$('#offset').val(offset);
 })();
 
 // Resource:
 // https://stackoverflow.com/questions/12742595/show-how-many-characters-remaining-in-a-html-text-box-using-javascript
 // textCounter keeps track of how many characters are typed into the message box by the user
-{
- var countfield = document.getElementById(field2);
- if ( field.value.length > maxlimit ) {
-  field.value = field.value.substring( 0, maxlimit );
-  return false;
- } else {
-  countfield.value = maxlimit - field.value.length;
- }
+function textCounter(field, field2, maxlimit) {
+	var countfield = document.getElementById(field2);
+	if (field.value.length > maxlimit) {
+		field.value = field.value.substring(0, maxlimit);
+		return false;
+	} else {
+		countfield.value = maxlimit - field.value.length;
+	}
 }
-
